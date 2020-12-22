76ff02ccf89f87a8e2f982470260eee26e2b03f2
engindo <engingulcicek@icloud.com>
2020-12-22 17:20:57 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,24 @@
+ScrollReveal().reveal('.ani-left',{
+    origin: "left",
+    distance: "10rem",
+    duration: "1000",
+    easing: "ease-in"
+});
+ScrollReveal().reveal('.ani-right',{
+    origin: "right",
+    distance: "10rem",
+    duration: "1000",
+    easing: "ease-in"
+});
+ScrollReveal().reveal('.ani-leftt',{
+    origin: "left",
+    distance: "10rem",
+    duration: "1500",
+    easing: "ease-in"
+});
+ScrollReveal().reveal('.ani-delay',{
+    delay: 500,
+});
+ScrollReveal().reveal('.ani-delay1',{
+    delay: 1000,
+});
